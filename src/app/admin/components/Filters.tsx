cb09eb16056a2bda70649e716b54d99afa93518a
QuocAnU <an.lequoc2103@hcmut.edu.vn>
2024-11-30 21:50:28 +0700

    fix deploy

--- src/app/admin/components/Filters.tsx
+++ src/app/admin/components/Filters.tsx
@@ -6,15 +6,9 @@ const { Option } = Select;
 interface FiltersProps {
     searchText: string;
     setSearchText: (text: string) => void;
-    statusFilter: string | undefined;
-    setStatusFilter: (status: string | undefined) => void;
-    totalRange: number[];
-    setTotalRange: (range: number[]) => void;
-    profitRange: number[];
-    setProfitRange: (range: number[]) => void;
-    profit: boolean;
-    total: boolean;
-    status: boolean;
+    statusFilter?: string | undefined;
+    setStatusFilter?: (status: string | undefined) => void;
+    status?: boolean;
     text: string;
 }
 
@@ -23,12 +17,6 @@ const Filters: React.FC<FiltersProps> = ({
     setSearchText,
     statusFilter,
     setStatusFilter,
-    totalRange,
-    setTotalRange,
-    profitRange,
-    setProfitRange,
-    profit,
-    total,
     status,
     text,
 }) => {
@@ -60,44 +48,6 @@ const Filters: React.FC<FiltersProps> = ({
                     </Select>
                 }
             </div>
-
-            {/* Total Range Filter */}
-            {
-                total &&
-                <Input.Group compact style={{ marginBottom: 16 }}>
-                    <Input
-                        style={{ width: '50%' }}
-                        placeholder="Min Total"
-                        type="number"
-                        onChange={e => setTotalRange([+e.target.value, totalRange[1]])}
-                    />
-                    <Input
-                        style={{ width: '50%' }}
-                        placeholder="Max Total"
-                        type="number"
-                        onChange={e => setTotalRange([totalRange[0], +e.target.value])}
-                    />
-                </Input.Group>
-            }
-
-            {/* Profit Range Filter */}
-            {
-                profit &&
-                <Input.Group compact style={{ marginBottom: 16 }}>
-                    <Input
-                        style={{ width: '50%' }}
-                        placeholder="Min Profit"
-                        type="number"
-                        onChange={e => setProfitRange([+e.target.value, profitRange[1]])}
-                    />
-                    <Input
-                        style={{ width: '50%' }}
-                        placeholder="Max Profit"
-                        type="number"
-                        onChange={e => setProfitRange([profitRange[0], +e.target.value])}
-                    />
-                </Input.Group>
-            }
         </div>
     );
 };
