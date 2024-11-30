ab9efa0903f2ce7ec49c5a7c5624214bafec540b
QuocAnU <an.lequoc2103@hcmut.edu.vn>
2024-11-30 21:18:34 +0700

    fix deploy

---
+++ src/utils/formatCurrency.ts
@@ -0,0 +1,8 @@
+// utils/formatCurrency.ts
+export const formatCurrency = (amount: number): string => {
+  return new Intl.NumberFormat("vi-VN", {
+    style: "currency",
+    currency: "VND",
+    maximumFractionDigits: 0,
+  }).format(amount);
+};
