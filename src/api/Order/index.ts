beba8e3a91b59616d12fdefc9e4003375c99efcd
QuocAnU <an.lequoc2103@hcmut.edu.vn>
2024-12-06 10:09:32 +0700

    api order

--- src/api/Order/index.ts
+++ src/api/Order/index.ts
@@ -1,10 +1,20 @@
-import { GET } from "../APIInstance";
+import { GET, POST } from "../APIInstance";
 
 const URL = 'https://api-tmdt.onrender.com/api/v1/orders';
+
+interface OrderCheckout {
+    quantity: number,
+    priceAtTime: number,
+    productID: string,
+}
 
 const OrderApi = {
     getAll: async (token: string | null) => {
         return await GET(URL, token, '');
+    },
+
+    create: async (token: string | null, data: OrderCheckout[]) => {
+        return await POST(`${URL}/user/orders`, token, "", data);
     },
 
     getOrderDetail: async (token: string | null, id: string) => {
